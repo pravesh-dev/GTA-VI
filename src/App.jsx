946c01a39a81e72e66e717dd062d76c5afa76eaa
pravesh dav <humantorch1932@gmail.com>
2025-05-09 15:24:30 +0530

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,90 @@
 import React, { useState } from "react";
 import { useGSAP } from "@gsap/react";
-import gsap from 'gsap'
+import gsap from "gsap";
 
 const App = () => {
   let [isContent, setIsContent] = useState(false);
 
-  useGSAP(()=> {
+  useGSAP(() => {
     const tl = gsap.timeline();
 
-    tl.to('.vi-mask-group', {
+    tl.to(".vi-mask-group", {
       rotate: 10,
       duration: 2,
-      ease: 'power3.inOut',
-      transformOrigin: '50% 50%'
-    })
-    .to('.vi-mask-group', {
+      ease: "power3.inOut",
+      transformOrigin: "50% 50%",
+    }).to(".vi-mask-group", {
       scale: 26,
       duration: 2,
       delay: -1.8,
-      ease: 'Expo.easeInOut',
-      transformOrigin: '50% 50%',
+      ease: "Expo.easeInOut",
+      transformOrigin: "50% 50%",
       opacity: 0,
-      onUpdate: function(){
-        if(this.progress() >= 0.9){
-          document.querySelector('.svg').remove();
+      onUpdate: function () {
+        if (this.progress() >= 0.9) {
+          document.querySelector(".svg").remove();
           setIsContent(true);
           this.kill();
         }
-      }
-    })
-  })
+      },
+    });
+  });
   return (
-    <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
-      <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
-        <defs>
-          <mask id="viMask">
-            <rect width="100%" height="100%" fill="black" />
-            <g className="vi-mask-group">
-              <text
-                x="50%"
-                y="50%"
-                fontSize="250"
-                textAnchor="middle"
-                fill="white"
-                dominantBaseline="middle"
-                fontFamily="Arial Black"
-              >
-                VI
-              </text>
-            </g>
-          </mask>
-        </defs>
-        <image
-          href="./bg2-trasparent.png"
-          width="100%"
-          height="100%"
-          y="-10%"
-          preserveAspectRatio="xMidYMid slice"
-          mask="url(#viMask)"
-        ></image>
-      </svg>
-    </div>
+    <>
+      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+        <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+          <defs>
+            <mask id="viMask">
+              <rect width="100%" height="100%" fill="black" />
+              <g className="vi-mask-group">
+                <text
+                  x="50%"
+                  y="50%"
+                  fontSize="250"
+                  textAnchor="middle"
+                  fill="white"
+                  dominantBaseline="middle"
+                  fontFamily="Arial Black"
+                >
+                  VI
+                </text>
+              </g>
+            </mask>
+          </defs>
+          <image
+            href="./bg2-trasparent.png"
+            width="100%"
+            height="100%"
+            y="-10%"
+            preserveAspectRatio="xMidYMid slice"
+            mask="url(#viMask)"
+          ></image>
+        </svg>
+      </div>
+      {isContent && (
+        <div className="w-full">
+          <div className="landing w-full h-screen bg-black">
+            <div className="imagesdiv w-full h-screen relative">
+              <img
+                className="w-full h-full object-cover absolute top-0 left-0"
+                src="./sky2.png"
+                alt="sky image"
+              />
+              <img
+                className="w-full h-full object-cover absolute top-0 left-0"
+                src="./bg2-trasparent.png"
+                alt="background image"
+              />
+              <img
+                className="h-full absolute bottom-0 right-0"
+                src="./girlbg.png"
+                alt="girl image"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
